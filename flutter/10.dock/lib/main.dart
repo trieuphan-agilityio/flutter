1b74757f7edfbaac862e12f9c2db2855c8628a67
Sang <sang.dongvan@asnet.com.vn>
2020-04-20 17:51:35 +0700

    first working horizontal layout (need to fix issue immutability of the widget)

--- flutter/10.dock/lib/main.dart
+++ flutter/10.dock/lib/main.dart
@@ -10,12 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }

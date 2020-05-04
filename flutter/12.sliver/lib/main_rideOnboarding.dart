049a7c5f089b850ccf9c82dfe5da7179f2dd83fd
Sang <sang.dongvan@asnet.com.vn>
2020-05-04 16:22:52 +0700

    home layout wip

--- flutter/12.sliver/lib/main_rideOnboarding.dart
+++ flutter/12.sliver/lib/main_rideOnboarding.dart
@@ -1,8 +1,15 @@
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 main() {
-  runApp(MaterialApp(home: _Demo()));
+  runApp(MaterialApp(
+    theme: ThemeData(
+      primaryColor: Colors.black,
+      primaryColorLight: Colors.white,
+    ),
+    home: _Demo(),
+  ));
 }
 
 class _Demo extends StatelessWidget {
@@ -12,14 +19,30 @@ class _Demo extends StatelessWidget {
       map: Container(
           color: Colors.blueGrey,
           child: Container(decoration: FlutterLogoDecoration())),
+      appBar: AppBar(
+        leading: Icon(Icons.menu),
+        backgroundColor: Colors.transparent,
+      ),
       input: Card(
+        margin: EdgeInsets.symmetric(horizontal: 16),
         child: ListTile(
           leading: Icon(Icons.location_on),
           title: Text('Where to?'),
         ),
       ),
       locationRefresher: LocationRefresher(),
-      feedDrawer: Feed(),
+      promoBanner: const PromoBanner(),
+      sliverFeedItems: <Widget>[
+        SliverToBoxAdapter(child: FeedDivider()),
+        SliverToBoxAdapter(child: FeedSingleItem()),
+        SliverToBoxAdapter(child: FeedDivider()),
+        SliverToBoxAdapter(child: FeedMultiItem()),
+        SliverToBoxAdapter(child: FeedDivider()),
+        SliverToBoxAdapter(child: FeedSingleItem()),
+        SliverToBoxAdapter(child: FeedDivider()),
+        SliverToBoxAdapter(child: FeedMultiItem()),
+        SliverToBoxAdapter(child: FeedDivider()),
+      ],
     );
   }
 }
@@ -30,16 +53,20 @@ class _Demo extends StatelessWidget {
 
 class Home extends StatefulWidget {
   final Widget map;
+  final Widget appBar;
   final Widget input;
   final Widget locationRefresher;
-  final Widget feedDrawer;
+  final Widget promoBanner;
+  final List<Widget> sliverFeedItems;
 
   const Home({
     Key key,
     @required this.map,
+    this.appBar,
     this.input,
     this.locationRefresher,
-    this.feedDrawer,
+    this.promoBanner,
+    this.sliverFeedItems,
   }) : super(key: key);
 
   @override
@@ -49,75 +76,29 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    final List<LayoutId> children = <LayoutId>[];
-
-    _addIfNonNull(children, widget.map, _HomeSlot.map);
-    _addIfNonNull(
-      children,
-      _FeedController(
-        child: widget.feedDrawer,
-      ),
-      _HomeSlot.feedDrawer,
+    return Stack(
+      children: <Widget>[
+        Positioned.fill(child: widget.map),
+        Align(
+          alignment: Alignment.topCenter,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              widget.appBar,
+              widget.input,
+            ],
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: _FeedController(
+            promoBanner: widget.promoBanner,
+            sliverFeedItems: widget.sliverFeedItems,
+          ),
+        ),
+      ],
     );
-
-    return CustomMultiChildLayout(
-      children: children,
-      delegate: _HomeLayout(),
-    );
   }
-
-  void _addIfNonNull(
-    List<LayoutId> children,
-    Widget child,
-    Object childId,
-  ) {
-    MediaQueryData data = MediaQuery.of(context);
-    if (child != null) {
-      children.add(LayoutId(
-        id: childId,
-        child: MediaQuery(data: data, child: child),
-      ));
-    }
-  }
-}
-
-enum _HomeSlot {
-  map,
-  input,
-  locationRefresher,
-  feedDrawer,
-}
-
-class _HomeLayout extends MultiChildLayoutDelegate {
-  @override
-  void performLayout(Size size) {
-    final BoxConstraints looseConstraints = BoxConstraints.loose(size);
-    final BoxConstraints fullWidthConstraints =
-        looseConstraints.tighten(width: size.width);
-
-    if (hasChild(_HomeSlot.map)) {
-      layoutChild(_HomeSlot.map, BoxConstraints.tight(size));
-      positionChild(_HomeSlot.map, Offset.zero);
-    }
-
-    if (hasChild(_HomeSlot.input)) {
-      layoutChild(_HomeSlot.input, fullWidthConstraints);
-      positionChild(_HomeSlot.input, Offset.zero);
-    }
-
-    if (hasChild(_HomeSlot.locationRefresher)) {
-      layoutChild(_HomeSlot.locationRefresher, fullWidthConstraints);
-      positionChild(_HomeSlot.locationRefresher, Offset.zero);
-    }
-
-    if (hasChild(_HomeSlot.feedDrawer)) {
-      layoutChild(_HomeSlot.feedDrawer, BoxConstraints.tight(size));
-      positionChild(_HomeSlot.feedDrawer, Offset.zero);
-    }
-  }
-
-  @override
-  bool shouldRelayout(MultiChildLayoutDelegate oldDelegate) => false;
 }
 
 class LocationRefresher extends StatelessWidget {
@@ -151,13 +132,18 @@ typedef _FeedDrawerCallback = void Function(bool isOpened);
 class _FeedController extends StatefulWidget {
   const _FeedController({
     GlobalKey key,
-    @required this.child,
+    this.promoBanner,
+    this.sliverFeedItems,
     this.feedDrawerCallback,
     this.edgeDragHeight,
-  }) : super(key: key);
+  })  : assert(promoBanner != null),
+        assert(sliverFeedItems != null),
+        super(key: key);
 
   /// The widget below this widget in the tree.
-  final Widget child;
+  final Widget promoBanner;
+
+  final List<Widget> sliverFeedItems;
 
   /// Optional callback that is called when a [Drawer] is opened or closed.
   final _FeedDrawerCallback feedDrawerCallback;
@@ -172,13 +158,14 @@ class _FeedController extends StatefulWidget {
   _FeedControllerState createState() => _FeedControllerState();
 }
 
-const double _kHeight = 200.0;
-const double _kEdgeDragHeight = 50.0;
+const double _kEdgeDragHeight = 150.0;
 const double _kMinFlingVelocity = 365.0;
 const Duration _kBaseSettleDuration = Duration(milliseconds: 246);
 
 class _FeedControllerState extends State<_FeedController>
     with SingleTickerProviderStateMixin {
+  ScrollController _scrollController;
+
   @override
   void initState() {
     super.initState();
@@ -186,17 +173,22 @@ class _FeedControllerState extends State<_FeedController>
         AnimationController(duration: _kBaseSettleDuration, vsync: this)
           ..addListener(_animationChanged)
           ..addStatusListener(_animationStatusChanged);
+
+    _scrollController = ScrollController()..addListener(_scrollChanged);
   }
 
   @override
   void dispose() {
     _controller.dispose();
+    _scrollController.dispose();
     super.dispose();
   }
 
   void _animationChanged() {
     setState(() {
-      // The animation controller's state is our build state, and it changed already.
+      if (_scrollController.hasClients) {
+        _scrollController.jumpTo(_controller.value * 812.0);
+      }
     });
   }
 
@@ -223,7 +215,7 @@ class _FeedControllerState extends State<_FeedController>
     final RenderBox box =
         _drawerKey.currentContext?.findRenderObject() as RenderBox;
     if (box != null) return box.size.height;
-    return _kHeight;
+    return _kEdgeDragHeight;
   }
 
   bool _previouslyOpened = false;
@@ -263,14 +255,19 @@ class _FeedControllerState extends State<_FeedController>
   }
 
   final GlobalKey _gestureDetectorKey = GlobalKey();
+  final GlobalKey _customScrollKey = GlobalKey();
+
+  double backdropOpacity = 0.0;
+
+  void _scrollChanged() {
+    setState(() {
+      // when scrolling over 30% the backdrop turn to black completely
+      backdropOpacity = math.min(1.0, _scrollController.offset / (812.0 * 0.3));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    double dragAreaHeight = widget.edgeDragHeight;
-    if (widget.edgeDragHeight == null) {
-      dragAreaHeight = _kEdgeDragHeight;
-    }
-
     if (_controller.status == AnimationStatus.dismissed) {
       return Align(
           alignment: AlignmentDirectional.bottomCenter,
@@ -278,106 +275,149 @@ class _FeedControllerState extends State<_FeedController>
               key: _gestureDetectorKey,
               onVerticalDragUpdate: _move,
               onVerticalDragEnd: _settle,
-              child: Container(color: Colors.red, height: dragAreaHeight)));
+              child: widget.promoBanner));
     } else {
       return GestureDetector(
-          key: _gestureDetectorKey,
-          onVerticalDragDown: _handleDragDown,
-          onVerticalDragUpdate: _move,
-          onVerticalDragEnd: _settle,
-          onVerticalDragCancel: _handleDragCancel,
-          child: Stack(children: <Widget>[
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Align(
-                alignment: Alignment.topCenter,
-                heightFactor: _controller.value,
-                child: widget.child,
-              ),
-            ),
-          ]));
+        key: _gestureDetectorKey,
+        onVerticalDragDown: _handleDragDown,
+        onVerticalDragUpdate: _move,
+        onVerticalDragEnd: _settle,
+        onVerticalDragCancel: _handleDragCancel,
+        child: _buildCustomScrollView(),
+      );
     }
   }
-}
 
-class Feed extends StatefulWidget {
-  @override
-  _FeedState createState() => _FeedState();
-}
-
-class _FeedState extends State<Feed> {
-  var noScroll = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: <Widget>[
-        SliverAppBar(
-          backgroundColor: Colors.black,
-          pinned: true,
-          stretch: true,
-          expandedHeight: 300.0,
-          flexibleSpace: FlexibleSpaceBar(
-            title: const Text('Messages'),
-            centerTitle: false,
-            background: ColoredBox(color: Colors.black),
+  Widget _buildCustomScrollView() {
+    return RepaintBoundary(
+      child: CustomScrollView(
+        key: _customScrollKey,
+        controller: _scrollController,
+        slivers: <Widget>[
+          SliverPromoBanner(
+            child: Stack(
+              children: <Widget>[
+                Container(color: Theme.of(context).primaryColor),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: widget.promoBanner,
+                ),
+              ],
+            ),
           ),
-        ),
-        SliverToBoxAdapter(child: FeedDivider()),
-        SliverToBoxAdapter(child: FeedSingleItem()),
-        SliverToBoxAdapter(child: FeedDivider()),
-        SliverToBoxAdapter(child: FeedMultiItem()),
-        SliverToBoxAdapter(child: FeedDivider()),
-        SliverToBoxAdapter(child: FeedSingleItem()),
-        SliverToBoxAdapter(child: FeedDivider()),
-        SliverToBoxAdapter(child: FeedMultiItem()),
-        SliverToBoxAdapter(child: FeedDivider()),
-      ],
+          ...widget.sliverFeedItems,
+        ],
+      ),
     );
   }
 }
 
-class PromoBanner extends SingleChildRenderObjectWidget {
-  /// Creates a sliver that contains a single box widget.
-  const PromoBanner({Key key, Widget child}) : super(key: key, child: child);
+/// ===================================================================
+/// Feed Header
+/// ===================================================================
+
+class SliverPromoBanner extends SingleChildRenderObjectWidget {
+  SliverPromoBanner({Key key, Widget child}) : super(key: key, child: child);
 
   @override
   RenderSliverPromoBanner createRenderObject(BuildContext context) =>
       RenderSliverPromoBanner();
 }
 
-class RenderSliverPromoBanner extends RenderSliverToBoxAdapter {
+class RenderSliverPromoBanner extends RenderSliverSingleBoxAdapter {
+  /// Creates a [RenderSliver] that wraps a non-scrollable [RenderBox] which is
+  /// sized to fit the remaining space in the viewport.
+  RenderSliverPromoBanner({RenderBox child}) : super(child: child);
+
   @override
   void performLayout() {
-    if (child == null) {
-      geometry = SliverGeometry.zero;
-      return;
-    }
     final SliverConstraints constraints = this.constraints;
-    child.layout(constraints.asBoxConstraints(), parentUsesSize: true);
-    double childExtent;
-    switch (constraints.axis) {
-      case Axis.horizontal:
-        childExtent = child.size.width;
-        break;
-      case Axis.vertical:
-        childExtent = child.size.height;
-        break;
-    }
-    assert(childExtent != null);
-    final double paintedChildSize =
-        calculatePaintOffset(constraints, from: 0.0, to: childExtent);
+    // The remaining space in the viewportMainAxisExtent. Can be <= 0 if we have
+    // scrolled beyond the extent of the screen.
+    double extent =
+        constraints.viewportMainAxisExtent - constraints.precedingScrollExtent;
 
+    if (child != null) {
+      double childExtent;
+      switch (constraints.axis) {
+        case Axis.horizontal:
+          childExtent = child.getMaxIntrinsicWidth(constraints.crossAxisExtent);
+          break;
+        case Axis.vertical:
+          childExtent =
+              child.getMaxIntrinsicHeight(constraints.crossAxisExtent);
+          break;
+      }
+
+      // If the childExtent is greater than the computed extent, we want to use
+      // that instead of potentially cutting off the child. This allows us to
+      // safely specify a maxExtent.
+      extent = math.max(extent, childExtent);
+      child.layout(constraints.asBoxConstraints(
+        minExtent: extent,
+        maxExtent: extent,
+      ));
+    }
+
+    assert(
+      extent.isFinite,
+      'The calculated extent for the child of SliverFillRemaining is not finite. '
+      'This can happen if the child is a scrollable, in which case, the '
+      'hasScrollBody property of SliverFillRemaining should not be set to '
+      'false.',
+    );
+    final double paintedChildSize =
+        calculatePaintOffset(constraints, from: 0.0, to: extent);
     assert(paintedChildSize.isFinite);
     assert(paintedChildSize >= 0.0);
     geometry = SliverGeometry(
-      scrollExtent: childExtent,
+      scrollExtent: extent,
       paintExtent: paintedChildSize,
-      maxPaintExtent: childExtent,
-      hasVisualOverflow: childExtent > constraints.remainingPaintExtent ||
+      maxPaintExtent: paintedChildSize,
+      hasVisualOverflow: extent > constraints.remainingPaintExtent ||
           constraints.scrollOffset > 0.0,
     );
-    setChildParentData(child, constraints, geometry);
+    if (child != null) setChildParentData(child, constraints, geometry);
+  }
+}
+
+class PromoBanner extends StatelessWidget {
+  const PromoBanner({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: _kEdgeDragHeight,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Container(
+              height: 60.0,
+              padding: EdgeInsets.symmetric(horizontal: 50),
+              decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                  )),
+              child: Text(
+                'UBER',
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6
+                    .copyWith(color: Theme.of(context).primaryColorLight),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: Container(height: 90.0, color: Colors.teal),
+          ),
+        ],
+      ),
+    );
   }
 }
 
@@ -389,12 +429,11 @@ class FeedSingleItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 250,
-      child: Card(
-        shape: BeveledRectangleBorder(),
-        margin: EdgeInsets.zero,
-      ),
-    );
+        height: 250,
+        child: Card(
+          shape: BeveledRectangleBorder(),
+          margin: EdgeInsets.zero,
+        ));
   }
 }
 
@@ -415,7 +454,8 @@ class FeedMultiItem extends StatelessWidget {
             ),
           );
         },
-        separatorBuilder: (_, __) => Container(width: 6, color: Colors.black),
+        separatorBuilder: (_, __) =>
+            Container(width: 6, color: Theme.of(context).primaryColor),
       ),
     );
   }
@@ -428,6 +468,6 @@ class FeedMultiItem extends StatelessWidget {
 class FeedDivider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(height: 6, color: Colors.black);
+    return Container(height: 6, color: Theme.of(context).primaryColor);
   }
 }
